0a2cca13059fd6cdf45b82f44638158af4e54f11
victorhuang1 <geekle@yeah.net>
2017-11-18 00:34:54 +0800

    challenge2

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@
 import sys
 
 def tax(x):
-    y=int(x)-3500
+    y=int(x)*(1-(8/100+2/100+0.5/100+0/100+0/100+6/100))-3500
     if y<0:
         z=0
     elif y<=1500 and y>=0:
@@ -22,11 +22,15 @@ def tax(x):
         z=y*45/100-13505
     else:
         print("out of range")
-    print(format(z,'.2f'))
+    return (format(((y+3500)-z),'.2f'))
 
 try:
-    x=sys.argv[1]
-    int(x)
-    tax(x)
+    for arg in sys.argv[1:]:
+        info=arg.split(':')
+        gross_income=int(info[1])
+        int(gross_income)
+        dispensable_income=tax(gross_income)
+        print(info[0]+':'+dispensable_income)
+
 except ValueError:
     print("Parameter Error")
